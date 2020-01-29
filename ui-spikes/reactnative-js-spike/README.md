1da3415260f318e0ca4b081e768bc0b2430df740
Maciej Falski <takaczapka@gmail.com>
2020-01-29 17:15:13 +0000

    Add i18n support

--- ui-spikes/reactnative-js-spike/README.md
+++ ui-spikes/reactnative-js-spike/README.md
@@ -1,17 +1,25 @@
 
+# Introduction
+The spike is based on Expo (http://expo.io) framework which is "Ruby on Rails"-like set of tools for ReactNative applications.
 
 # Prerequisites
-`npm install expo-cli --global`
+```
+npm install expo-cli --global
+expo install expo-localization
+```
 
-# Template
-Generated with:
+# Template created with
 ```
 expo init pariyatti
 # option: `blank`
 ```
 
-# Start the app
+# Running the app
 ```
 cd pariyatti
 npm start
+
 ```
+
+(iOS only for now)
+This will start Expo console web app and open a link in the browser. Choose "Run iOS simulator" to see the app in OSX IPhone Simulator. Alternatively publish it in Echo profile and interact with the app in IPhone via iOS Expo app.  
--- ui-spikes/reactnative-js-spike/pariyatti/App.js
+++ ui-spikes/reactnative-js-spike/pariyatti/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
+
+// Set the key-value pairs for the different languages you want to support.
+i18n.translations = {
+  en: { welcome: 'Hello' },
+  ja: { welcome: 'こんにちは' },
+  pl: { welcome: "Cześć" },
+  hi: { welcome: "नमस्ते" }
+};
+// Set the locale once at the beginning of your app.
+i18n.locale = Localization.locale;
+// When a value is missing from a language it'll fallback to another language with the key present.
+i18n.fallbacks = true;
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Pariyatti!</Text>
+      <Text>{i18n.t('welcome')} Pariyatti!</Text>
     </View>
   );
 }
